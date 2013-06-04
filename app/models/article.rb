cf9588da095a819e7a43ef523ffb1a2cc65d19a8
Jonathan Lai <jonathanlai@eltux.net>
2013-06-03 22:44:44 -0700

    has_man belong to relationships added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ActiveRecord::Base
   attr_accessible :body, :title
+  has_many :comments, :dependent => :destroy
+
 
   validates_presence_of :title, :body
   validates_uniqueness_of :title
+
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   attr_accessible :article_id, :body, :email, :name
+  belongs_to :article
+
+
+
 end
