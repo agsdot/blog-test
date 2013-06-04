c8d8f7a514fb581f698d75f3a530de668c009862
Jonathan Lai <jonathanlai@eltux.net>
2013-06-03 22:43:23 -0700

    added scaffold for comments and rake db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Blog::Application.routes.draw do
+  resources :comments
+
+
   resources :articles
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604053857) do
+ActiveRecord::Schema.define(:version => 20130604054258) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "article_id"
+    t.string   "name"
+    t.string   "email"
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
